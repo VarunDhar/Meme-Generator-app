cd51a5b404b8001ed2b6f8fb8efea8819f670d71
VarunDhar <85066986+VarunDhar@users.noreply.github.com>
2023-09-28 18:01:55 +0530

    Update Tag.js
    optimised the code using custom hooks and removed comments

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -3,24 +3,6 @@ import useGif from '../hooks/useGif';
 import { Spinner } from './Spinner';
 
 export const Tag = () => {
-//   const [gif,setGif] = useState('');
-//   const [isLoading, setLoading] = useState(false);
-//   const [input,setInput] = useState("");
-
-// const key = process.env.REACT_APP_API_KEY;
-// const url = `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=${input}`;
-
-
-
-//   async function fetchData(){
-//     setLoading(true);
-//     const {data} = await axios.get(url);
-//     setGif( data.data.images.downsized.url);
-//     setLoading(false);
-//   }
-//   useEffect(()=>{
-//     fetchData();  
-//   },[]);
 
   const {gif,fetchData,isLoading,input,setInput} = useGif();
 
